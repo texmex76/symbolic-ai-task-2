96eed4515ee00a389ad45ac13227d843dcce4059
Bernhard Gstrein <bernhard-gstrein@mailbox.org>
2020-05-27 14:09:10 +0200

    Work more on append

--- our_examples/append/app.b
+++ our_examples/append/app.b
@@ -3,12 +3,20 @@
 %:- modeb(*,app(+any,+any,-any)).
 %:- modeb(1,((+list) = ([-any|-list]))).
 
-:- mode(*,append(+listy,+any,+any)).
+:- mode(*,append(+strict_listy,+listy,+listy)).
+
+:- mode(1,((+strict_listy) = ([-any|-listy]))).
+:- mode(1,((+strict_listy) = ([]))).
+
 :- mode(1,((+listy) = ([-any|-listy]))).
 :- mode(1,((+listy) = ([]))).
+:- mode(1,((+listy) = (+any))).
 
-%:- set(i,3).
-%:- set(noise,0).
+%:- set(clauselength,3).
+:- set(depth,3).
+
+%:- set(i,4).
+:- set(noise,0).
 
 
 :- determination(append/3,append/3).
--- our_examples/append/app.f
+++ our_examples/append/app.f
@@ -1,7 +1,23 @@
+% append([],Xs,Xs).
+% append([X|Xs],Ys,[X|Zs]) :-
+%    append(Xs,Ys,Zs).
+
+% Learning the base clause
 append([],0,0).
 append([],1,1).
 append([],2,2).
-append([],3,3).
-append([5|[]],3,[5|3]).
-append([5|[]],2,[5|2]).
-append([5|[]],1,[5|1]).
+
+append([1,2,3],[4],[1,2,3,4]).
+append([2,3],[4],[2,3,4]).
+append([3],[4],[3,4]).
+append([],[4],[4]).
+
+append([1,2,3],4,[1,2,3|4]).
+append([2,3],4,[2,3|4]).
+append([3],4,[3|4]).
+append([],4,4).
+
+append([1,2,3],5,[1,2,3|5]).
+append([2,3],5,[2,3|5]).
+append([3],5,[3|5]).
+append([],5,5).
